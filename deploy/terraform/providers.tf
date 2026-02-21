a1cdb672a9fe461cc7b5b8871f4ed103f58192cb
michael anikammadu <dil.anikamadu@gmail.com>
2026-02-21 16:53:54 +0100

    tf backend

--- deploy/terraform/providers.tf
+++ deploy/terraform/providers.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "gcs" {
+    bucket = "open-telemetry-1000-terraform-state"
+    prefix = "terraform/state"
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = "us-central1"
