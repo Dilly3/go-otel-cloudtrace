c73deb116cedbcd14a2fc4123d2ed11780aa3e5c
michael anikammadu <dil.anikamadu@gmail.com>
2026-02-21 16:57:56 +0100

    add permission

--- deploy/terraform/service_account.tf
+++ deploy/terraform/service_account.tf
@@ -28,6 +28,7 @@ resource "google_project_iam_member" "sa_roles" {
     "roles/artifactregistry.admin",   # For Artifact Registry
     "roles/artifactregistry.writer",  # For Artifact Registry
     "roles/artifactregistry.reader",  # For Artifact Registry
+    "roles/storage.objectAdmin", # Storage Object
   ])
   project = var.project_id
   role    = each.key
